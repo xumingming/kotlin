ea4c58042e9a09b6d5b1d9210b5220914dca7b20
Andrey Breslav <andrey.breslav@jetbrains.com>
2015-04-30 09:23:22 +0300

    ++ test data fixed

--- compiler/testData/diagnostics/tests/platformTypes/methodCall/multipleExactBoundsNullable.kt
+++ compiler/testData/diagnostics/tests/platformTypes/methodCall/multipleExactBoundsNullable.kt
@@ -1,3 +1,5 @@
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+
 import java.util.HashMap
 
 trait ResolverForProject<M1> {
@@ -5,18 +7,18 @@ trait ResolverForProject<M1> {
 }
 
 class ResolverForProjectImpl<M>(
-        <!UNUSED_PARAMETER!>descriptorByModule<!>: Map<M, String>,
-        <!UNUSED_PARAMETER!>delegateResolver<!>: ResolverForProject<M>
+        descriptorByModule: Map<M, String>,
+        delegateResolver: ResolverForProject<M>
 ) : ResolverForProject<M>
 
 trait WithFoo {
     fun foo()
 }
 
-fun <M2: WithFoo> foo(delegateResolver: ResolverForProject<M2?>): ResolverForProject<M2> {
-    val descriptorByModule = HashMap<M2, String>()
+fun <M2: WithFoo> foo(delegateResolver: ResolverForProject<M2?>): ResolverForProject<M2?> {
+    val descriptorByModule = HashMap<M2?, String>()
     val result = ResolverForProjectImpl(descriptorByModule, delegateResolver)
-    result.exposeM.foo() // M is not M2?
+    result.exposeM<!UNSAFE_CALL!>.<!>foo() // M is not M2?
     result.exposeM?.foo() // no warning, M is not M2, hense M is M2!
 
     return ResolverForProjectImpl(descriptorByModule, delegateResolver) // another bound check
