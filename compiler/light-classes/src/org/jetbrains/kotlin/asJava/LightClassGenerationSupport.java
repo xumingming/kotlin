2e972c2e61b29d0d28e410f5b75046a898f834b8
Dmitry Petrov <Dmitry.Petrov@jetbrains.com>
2015-07-30 15:54:18 +0300

    Light class generation for new facades/parts. 1st working (naive) implementation.

--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/LightClassGenerationSupport.java
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/LightClassGenerationSupport.java
@@ -71,4 +71,13 @@ public abstract class LightClassGenerationSupport {
 
     @NotNull
     public abstract Collection<PsiClass> getPackageClasses(@NotNull FqName packageFqName, @NotNull GlobalSearchScope scope);
+
+    @NotNull
+    public abstract Collection<PsiClass> getFacadeClasses(@NotNull FqName facadeFqName, @NotNull GlobalSearchScope scope);
+
+    @NotNull
+    public abstract Collection<JetFile> findFilesForFacade(@NotNull FqName facadeFqName, @NotNull GlobalSearchScope scope);
+
+    @NotNull
+    public abstract LightClassConstructionContext getContextForFacade(@NotNull Collection<JetFile> files);
 }
