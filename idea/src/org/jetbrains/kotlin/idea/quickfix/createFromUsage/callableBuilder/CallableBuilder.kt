e7a3192e37a0e1e4f10cf4f3f1b72ec9dad0526c
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-09-21 20:31:25 +0300

    Drop some usages of KotlinBuiltIns.getInstance() in idea module

--- idea/src/org/jetbrains/kotlin/idea/quickfix/createFromUsage/callableBuilder/CallableBuilder.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/createFromUsage/callableBuilder/CallableBuilder.kt
@@ -34,7 +34,6 @@ import com.intellij.psi.*
 import com.intellij.psi.codeStyle.JavaCodeStyleManager
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.IncorrectOperationException
-import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.cfg.pseudocode.Pseudocode
 import org.jetbrains.kotlin.cfg.pseudocode.getContainingPseudocode
 import org.jetbrains.kotlin.descriptors.*
@@ -184,7 +183,7 @@ class CallableBuilder(val config: CallableBuilderConfiguration) {
             }
 
             if (newTypes.isEmpty()) {
-                newTypes.add(EqWrapper(KotlinBuiltIns.getInstance().getAnyType()))
+                newTypes.add(EqWrapper(currentFileModule.builtIns.anyType))
             }
 
             newTypes.map { TypeCandidate(it._type, scope) }.reverse()
@@ -954,8 +953,7 @@ class CallableBuilder(val config: CallableBuilderConfiguration) {
                 is JetProperty -> {
                     if (!declaration.hasInitializer() && containingElement is JetBlockExpression) {
                         val defaultValueType = typeCandidates[callableInfo.returnTypeInfo]!!.firstOrNull()?.theType
-                                               ?: KotlinBuiltIns.getInstance().getAnyType()
-                        val defaultValue = CodeInsightUtils.defaultInitializer(defaultValueType) ?: "null"
+                        val defaultValue = defaultValueType?.let {CodeInsightUtils.defaultInitializer(it) } ?: "null"
                         val initializer = declaration.setInitializer(JetPsiFactory(declaration).createExpression(defaultValue))!!
                         val range = initializer.getTextRange()
                         selectionModel.setSelection(range.getStartOffset(), range.getEndOffset())
