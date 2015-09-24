d8e2c702a456f9a7301b20e7a1b1659398437fa9
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-09-24 18:43:18 +0300

    ~ draft

--- core/deserialization/src/org/jetbrains/kotlin/serialization/deserialization/descriptors/DeserializedClassDescriptor.kt
+++ core/deserialization/src/org/jetbrains/kotlin/serialization/deserialization/descriptors/DeserializedClassDescriptor.kt
@@ -149,6 +149,8 @@ public class DeserializedClassDescriptor(
             }
         }
 
+        result.addAll(c.components.additionalSupertypesProvider.forClass(this))
+
         if (unresolved.isNotEmpty()) {
             c.components.errorReporter.reportIncompleteHierarchy(this, unresolved.map(DeserializedType::getPresentableText))
         }
