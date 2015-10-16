5f52eff759a7a5163e18b800acccf00a26c8c507
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-10-17 00:22:50 +0300

    Build stubs for modifiers

--- idea/testData/decompiler/decompiledText/Modifiers.expected.kt
+++ idea/testData/decompiler/decompiledText/Modifiers.expected.kt
@@ -4,5 +4,8 @@
 package test
 
 public final data class Modifiers public constructor() {
+    public final inline fun inlined(crossinline arg1: () -> kotlin.Unit, noinline arg2: () -> kotlin.Unit): kotlin.Unit { /* compiled code */ }
+
+    public final tailrec fun sum(x: kotlin.Long, sum: kotlin.Long): kotlin.Long { /* compiled code */ }
 }
 
