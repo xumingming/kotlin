845eaabc4d782502ffdabcb6ed728abd7aae8cc4
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-10-16 21:02:12 +0300

    Drop @inline from tests and incremental compilation

--- compiler/backend/src/org/jetbrains/kotlin/codegen/inline/inlineCodegenUtils.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inline/inlineCodegenUtils.kt
@@ -19,13 +19,21 @@ package org.jetbrains.kotlin.codegen.inline
 import org.jetbrains.kotlin.codegen.state.JetTypeMapper
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptorWithSource
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.load.kotlin.FileBasedKotlinClass
 import org.jetbrains.kotlin.load.kotlin.KotlinJvmBinaryPackageSourceElement
 import org.jetbrains.kotlin.load.kotlin.KotlinJvmBinarySourceElement
 import org.jetbrains.kotlin.load.kotlin.VirtualFileKotlinClass
+import org.jetbrains.kotlin.load.kotlin.header.KotlinClassHeader
 import org.jetbrains.kotlin.load.kotlin.incremental.components.IncrementalCache
 import org.jetbrains.kotlin.resolve.source.PsiSourceElement
+import org.jetbrains.kotlin.serialization.Flags
+import org.jetbrains.kotlin.serialization.ProtoBuf
+import org.jetbrains.kotlin.serialization.deserialization.NameResolver
+import org.jetbrains.kotlin.serialization.deserialization.TypeTable
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedCallableMemberDescriptor
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedSimpleFunctionDescriptor
+import org.jetbrains.kotlin.serialization.jvm.BitEncoding
+import org.jetbrains.kotlin.serialization.jvm.JvmProtoBufUtil
 
 public val FunctionDescriptor.sourceFilePath: String
     get() {
@@ -61,4 +69,48 @@ public fun FunctionDescriptor.getClassFilePath(typeMapper: JetTypeMapper, cache:
             cache.getClassFilePath(className)
         }
     }
+}
+
+public fun inlineFunctionsJvmNames(bytes: ByteArray): Set<String> {
+    val header = readKotlinHeader(bytes)
+    val annotationData = header.annotationData
+    val strings = header.strings
+
+    if (annotationData == null || strings == null) return emptySet()
+
+    return when (header.kind) {
+        KotlinClassHeader.Kind.CLASS -> {
+            val classData = JvmProtoBufUtil.readClassDataFrom(BitEncoding.decodeBytes(annotationData), strings)
+            inlineFunctionsJvmNames(classData.classProto.functionList, classData.nameResolver, classData.classProto.typeTable)
+        }
+        KotlinClassHeader.Kind.FILE_FACADE,
+        KotlinClassHeader.Kind.MULTIFILE_CLASS,
+        KotlinClassHeader.Kind.MULTIFILE_CLASS_PART -> {
+            val packageData = JvmProtoBufUtil.readPackageDataFrom(BitEncoding.decodeBytes(annotationData), strings)
+            inlineFunctionsJvmNames(packageData.packageProto.functionList, packageData.nameResolver, packageData.packageProto.typeTable)
+        }
+        else -> emptySet<String>()
+    }
+}
+
+private fun inlineFunctionsJvmNames(functions: List<ProtoBuf.Function>, nameResolver: NameResolver, protoTypeTable: ProtoBuf.TypeTable): Set<String> {
+    val typeTable = TypeTable(protoTypeTable)
+    val inlineFunctions = functions.filter { Flags.IS_INLINE.get(it.flags) }
+    val jvmNames = inlineFunctions.map {
+        JvmProtoBufUtil.getJvmMethodSignature(it, nameResolver, typeTable)
+    }
+    return jvmNames.filterNotNull().toSet()
+}
+
+private fun readKotlinHeader(bytes: ByteArray): KotlinClassHeader {
+    var header: KotlinClassHeader? = null
+
+    FileBasedKotlinClass.create(bytes) { className, classHeader, innerClasses ->
+        header = classHeader
+        null
+    }
+
+    if (header == null) throw AssertionError("Could not read kotlin header from byte array")
+
+    return header!!
 }
