eec70c9eebb1f8bf0c535e7126b0cf9b4beab1ed
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-10-27 20:59:33 +0300

    Add light class test for annotation class

---
+++ compiler/testData/asJava/lightClasses/annotationClass.java
@@ -0,0 +1,14 @@
+@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface Anno {
+    int i();
+
+    int j();
+
+    java.lang.String value();
+
+    double d();
+
+    int[] ia();
+
+    int[] ia2();
+}
