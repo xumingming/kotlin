b4771395b17aa1fb1236bc9581c6301329512d5f
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-10-29 18:07:07 +0300

    Refactor light fields: move all implementation into one file and prettify code

--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/KotlinLightEnumConstant.kt
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/KotlinLightEnumConstant.kt
@@ -19,22 +19,3 @@ package org.jetbrains.kotlin.asJava
 import com.intellij.psi.*
 import org.jetbrains.kotlin.psi.KtEnumEntry
 
-class KotlinLightEnumConstant(
-        manager: PsiManager,
-        origin: KtEnumEntry,
-        enumConstant: PsiEnumConstant,
-        containingClass: PsiClass,
-        private val initializingClass: PsiEnumConstantInitializer?
-) : KotlinLightField<KtEnumEntry, PsiEnumConstant>(manager, origin, enumConstant, containingClass), PsiEnumConstant {
-    override fun copy() = KotlinLightEnumConstant(getManager()!!, getOrigin(), getDelegate(), getContainingClass()!!, initializingClass)
-
-    // NOTE: we don't use "delegation by" because the compiler would generate method calls to ALL of PsiEnumConstant members,
-    // but we need only members whose implementations are not present in KotlinLightField
-    override fun getArgumentList(): PsiExpressionList? = getDelegate().getArgumentList()
-    override fun getInitializingClass(): PsiEnumConstantInitializer? = initializingClass
-    override fun getOrCreateInitializingClass(): PsiEnumConstantInitializer =
-            initializingClass ?: throw UnsupportedOperationException("Can't create enum constant body: ${getDelegate().getName()}")
-    override fun resolveConstructor(): PsiMethod? = getDelegate().resolveConstructor()
-    override fun resolveMethod(): PsiMethod? = getDelegate().resolveMethod()
-    override fun resolveMethodGenerics(): JavaResolveResult = getDelegate().resolveMethodGenerics()
-}
