150b4b684a165bff6e5d46640f0d9160fcc3b177
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-11-07 16:25:36 +0300

    Drop preconditions with eager message.

--- libraries/stdlib/src/kotlin/util/Preconditions.kt
+++ libraries/stdlib/src/kotlin/util/Preconditions.kt
@@ -12,18 +12,6 @@ import java.lang.IllegalStateException
 public fun require(value: Boolean): Unit = require(value) { "Failed requirement" }
 
 /**
- * Throws an [IllegalArgumentException] with an optional [message] if the [value] is false.
- *
- * @sample test.collections.PreconditionsTest.failingRequireWithMessage
- */
-@Deprecated("Use require with lazy message instead.", ReplaceWith("require(value) { message }"))
-public fun require(value: Boolean, message: Any = "Failed requirement"): Unit {
-    if (!value) {
-        throw IllegalArgumentException(message.toString())
-    }
-}
-
-/**
  * Throws an [IllegalArgumentException] with the result of calling [lazyMessage] if the [value] is false.
  *
  * @sample test.collections.PreconditionsTest.failingRequireWithLazyMessage
@@ -40,20 +28,6 @@ public inline fun require(value: Boolean, lazyMessage: () -> Any): Unit {
  */
 public fun <T:Any> requireNotNull(value: T?): T = requireNotNull(value) { "Required value was null" }
 
-/**
- * Throws an [IllegalArgumentException] with the given [message] if the [value] is null. Otherwise
- * returns the not null value.
- *
- * @sample test.collections.PreconditionsTest.requireNotNull
- */
-@Deprecated("Use requireNotNull with lazy message instead.", ReplaceWith("requireNotNull(value) { message }"))
-public fun <T:Any> requireNotNull(value: T?, message: Any = "Required value was null"): T {
-    if (value == null) {
-        throw IllegalArgumentException(message.toString())
-    } else {
-        return value
-    }
-}
 
 /**
  * Throws an [IllegalArgumentException] with the result of calling [lazyMessage] if the [value] is null. Otherwise
@@ -76,18 +50,6 @@ public inline fun <T:Any> requireNotNull(value: T?, lazyMessage: () -> Any): T {
 public fun check(value: Boolean): Unit = check(value) { "Check failed" }
 
 /**
- * Throws an [IllegalStateException] with an optional [message] if the [value] is false.
- *
- * @sample test.collections.PreconditionsTest.failingCheckWithMessage
- */
-@Deprecated("Use check with lazy message instead.", ReplaceWith("check(value) { message }"))
-public fun check(value: Boolean, message: Any = "Check failed"): Unit {
-    if (!value) {
-        throw IllegalStateException(message.toString())
-    }
-}
-
-/**
  * Throws an [IllegalStateException] with the result of calling [lazyMessage] if the [value] is false.
  *
  * @sample test.collections.PreconditionsTest.failingCheckWithLazyMessage
@@ -104,21 +66,6 @@ public inline fun check(value: Boolean, lazyMessage: () -> Any): Unit {
  * returns the not null value.
  */
 public fun <T:Any> checkNotNull(value: T?): T = checkNotNull(value) { "Required value was null" }
-
-/**
- * Throws an [IllegalStateException] with the given [message] if the [value] is null. Otherwise
- * returns the not null value.
- *
- * @sample test.collections.PreconditionsTest.checkNotNull
- */
-@Deprecated("Use checkNotNull with lazy message instead.", ReplaceWith("checkNotNull(value) { message }"))
-public fun <T:Any> checkNotNull(value: T?, message: Any = "Required value was null"): T {
-    if (value == null) {
-        throw IllegalStateException(message.toString())
-    } else {
-        return value
-    }
-}
 
 /**
  * Throws an [IllegalStateException] with the result of calling [lazyMessage]  if the [value] is null. Otherwise
